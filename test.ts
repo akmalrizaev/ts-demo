716210057d816c8f527a01017d20180b9cceb8b2
Akmal Rizayev <akmal.rizayev@gmail.com>
2022-11-26 14:49:32 +0500

    base types

--- test.ts
+++ test.ts
@@ -1,1 +1,35 @@
 const a: number = 5;
+
+let b: string ='abcd'; 
+
+let c = true;
+
+const d: string[] =['abc','def','ghi'];
+
+let e:any = 5;
+e = 'abc';
+
+function test(a: string): string {
+	return ''
+}
+
+function countCoord(cord: {lat:number, long:number}) {
+
+}
+
+function printIt(id: number | string) {
+	if (typeof id === 'number') {
+		console.log(id.toString)
+	}
+
+	if (typeof id === 'string') {
+		console.log(id.toUpperCase)
+	}
+}
+
+const x:undefined = undefined;
+
+const z:null = null;
+
+
+
